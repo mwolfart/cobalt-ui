15379bc0903ee0fa4451e722043a832d72f2903b
Murilo Wolfart <murilo.wolfart@materialplus.io>
2024-10-25 14:29:03 -0300

    fix: remove dist files

--- dist/button/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
-
-type Props = {
-    variant: 'primary' | 'secondary' | 'outlined' | 'link';
-    onClick: () => void;
-} & ButtonHTMLAttributes<HTMLButtonElement> & PropsWithChildren;
-export declare const Button: ({ variant, onClick, children, className, ...props }: Props) => import("react/jsx-runtime").JSX.Element;
-export {};
--- dist/main.js
+++
@@ -1,4 +0,0 @@
-import { Button as r } from "./button/index.js";
-import './assets/main.css';export {
-  r as Button
-};
